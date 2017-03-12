1ace89e6aa097a2ea1a2835cb6761c7234b5ffcd
ljmatth1 <ljmatth1@asu.edu>
2017-03-12 00:16:10 -0700

    Test phone verification

--- app/src/main/java/com/skoovy/android/verifyMobileNumberActivity.java
+++ app/src/main/java/com/skoovy/android/verifyMobileNumberActivity.java
@@ -9,6 +9,29 @@ import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.app.Application;
+import android.content.Context;
+
+import com.nexmo.sdk.NexmoClient;
+import com.nexmo.sdk.core.client.ClientBuilderException;
+import com.nexmo.sdk.verify.client.VerifyClient;
+
+import com.nexmo.sdk.NexmoClient;
+import com.nexmo.sdk.core.client.ClientBuilderException;
+import com.nexmo.sdk.verify.client.VerifyClient;
+import com.nexmo.sdk.verify.event.UserObject;
+import com.nexmo.sdk.verify.event.VerifyClientListener;
 
 
 public class verifyMobileNumberActivity extends AppCompatActivity {
@@ -16,6 +39,11 @@ public class verifyMobileNumberActivity extends AppCompatActivity {
     Button button1;
     Button button2;
     TextView phoneNumber;
+
+    private VerifyClient verifyClient;
+    private NexmoClient nexmoClient;
+    private boolean verified;
+    private static final String TAG = "verify";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,16 +71,10 @@ public class verifyMobileNumberActivity extends AppCompatActivity {
 
     public void addListenerOnButton() {
 
-
-
         //button1 is the SEND SMS button
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-
-
-
 
 
                 Toast.makeText(getApplicationContext(), "THIS IS WHERE THE NEXMO API WOULD SEND PIN VIA SMS TO USER", Toast.LENGTH_LONG).show();
@@ -61,6 +83,17 @@ public class verifyMobileNumberActivity extends AppCompatActivity {
                 Intent intent1 = new Intent(verifyMobileNumberActivity.this, verificationCodeActivity.class);
                 //now make it happen
                 startActivity(intent1);
+
+                Context context = getApplicationContext();
+                try {
+                    nexmoClient = new NexmoClient.NexmoClientBuilder()
+                            .context(context)
+                            .applicationId(Constants.NEXMO_ID) //your App key
+                            .sharedSecretKey(Constants.SHARED_SECRET) //your App secret
+                            .build();
+                } catch (ClientBuilderException e) {
+                    e.printStackTrace();
+                }
 
             }
         });
@@ -77,7 +110,30 @@ public class verifyMobileNumberActivity extends AppCompatActivity {
 
     }
 
-
+    public VerifyClient getVerifyClient(boolean verifiedValue) {
+        verified = verifiedValue;
+        return this.verifyClient;
+    }
+    //@Override
+    public void onCreate() {
+        //super.onCreate();
+        acquireVerifyClient();
+    }
+    public void acquireVerifyClient() {
+        Context context = getApplicationContext();
+        try {
+            this.nexmoClient = new NexmoClient.NexmoClientBuilder()
+                    .context(context)
+                    .applicationId(getResources().getString(R.string.nexmo_application_id))
+                    .sharedSecretKey(getResources().getString(R.string.nexmo_shared_secret))
+                    .build();
+        } catch (ClientBuilderException e) {
+            e.printStackTrace();
+            Log.d(TAG, e.toString());
+            return;
+        }
+        this.verifyClient = new VerifyClient(nexmoClient);
+    }
 
 
 }
