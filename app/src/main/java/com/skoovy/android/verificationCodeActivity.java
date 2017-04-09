6a81e5bcc31206629a45c00c3197602a34550043
tjcole2 <tjcole2@asu.edu>
2017-04-08 17:07:20 -0700

    Modified verification code

--- app/src/main/java/com/skoovy/android/verificationCodeActivity.java
+++ app/src/main/java/com/skoovy/android/verificationCodeActivity.java
@@ -7,6 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
@@ -14,6 +15,21 @@ import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.nexmo.sdk.NexmoClient;
+import com.nexmo.sdk.core.client.ClientBuilderException;
+import com.nexmo.sdk.verify.client.VerifyClient;
+import com.nexmo.sdk.verify.event.Command;
+import com.nexmo.sdk.verify.event.CommandListener;
+import com.nexmo.sdk.verify.event.SearchListener;
+import com.nexmo.sdk.verify.event.UserObject;
+import com.nexmo.sdk.verify.event.UserStatus;
+import com.nexmo.sdk.verify.event.VerifyClientListener;
+import com.nexmo.sdk.verify.event.VerifyError;
+
+import java.io.IOException;
+
+import static com.skoovy.android.R.string.application_id;
+import static com.skoovy.android.R.string.username_or_email;
 
 public class verificationCodeActivity extends AppCompatActivity {
 
@@ -25,6 +41,12 @@ public class verificationCodeActivity extends AppCompatActivity {
 
     PinEntryEditText pinNumber;
 
+    public static NexmoClient nexmoClient = null;
+    public static VerifyClient verifyClient = null;
+    private String TAG = "vCodeActivity";
+
+    User user;
+
     Button button1; //main action button for this activity
     ImageButton button2; //backbutton
 
@@ -32,6 +54,46 @@ public class verificationCodeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_verification_code);
+
+        try {
+            nexmoClient = new NexmoClient.NexmoClientBuilder()
+                    .context(getApplicationContext())
+                    .applicationId(getString(R.string.application_id)) //your App key
+                    .sharedSecretKey(getString(R.string.shared_secret)) //your App secret
+                    .build();
+        } catch (ClientBuilderException e) {
+            e.printStackTrace();
+        }
+
+        verifyClient = new VerifyClient(nexmoClient);
+
+        verifyClient.addVerifyListener(new VerifyClientListener() {
+            @Override
+            public void onVerifyInProgress(final VerifyClient verifyClient, UserObject user) {
+                Log.d(TAG, "onVerifyInProgress for number: " + user.getPhoneNumber());
+            }
+
+            @Override
+            public void onUserVerified(final VerifyClient verifyClient, UserObject user) {
+                Log.d(TAG, "onUserVerified for number: " + user.getPhoneNumber());
+            }
+
+            @Override
+            public void onError(final VerifyClient verifyClient, final com.nexmo.sdk.verify.event.VerifyError errorCode, UserObject user) {
+                Log.d(TAG, "onError: " + errorCode + " for number: " + user.getPhoneNumber());
+            }
+
+            @Override
+            public void onException(final IOException exception) {}
+        });
+
+        Intent intent6 = getIntent();
+        user = (User)intent6.getSerializableExtra("user");
+
+        //logOut(user.getNexmoPhoneNumber(),user.getPhoneCountryCode());
+
+        //The verification text is sent at this point.
+        verifyClient.getVerifiedUser(user.getPhoneCountryCode(), user.getNexmoPhoneNumber());
 
         //get font asset
         Typeface centuryGothic = Typeface.createFromAsset(getAssets(), "fonts/Century Gothic.ttf");
@@ -69,12 +131,7 @@ public class verificationCodeActivity extends AppCompatActivity {
                     InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
                     imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
                 }
-                if (s.toString().equals("1234")) {
-                    Toast.makeText(verificationCodeActivity.this, "Success", Toast.LENGTH_SHORT).show();
-                } else if (s.length() == "1234".length()) {
-                    Toast.makeText(verificationCodeActivity.this, "Incorrect", Toast.LENGTH_SHORT).show();
-                    // txtPinEntry.setText(null);  //THIS WILL CLEAR THE PASSCODE ENTERED IF SO DESIRED
-                }
+
             }
         });
 
@@ -95,9 +152,9 @@ public class verificationCodeActivity extends AppCompatActivity {
 
                 //Detect empty field before allowing user to continue to next activity
 //                if (TextUtils.isEmpty(firstName)) {
-                    //firstName is empty
+                //firstName is empty
 //                    Toast.makeText(getApplicationContext(), "Please enter firstName", Toast.LENGTH_SHORT).show();
-                    //stopping the function from executing further
+                //stopping the function from executing further
 //                    return;
 //                }
 
@@ -108,14 +165,16 @@ public class verificationCodeActivity extends AppCompatActivity {
                 if (pinNumber.length() < 4){
                     Toast.makeText(verificationCodeActivity.this, "I'm afraid that's not a valid pin number", Toast.LENGTH_SHORT).show();
                     return;
+                } else {
+                    //declare where you intend to go
+                    Intent intent5 = new Intent(verificationCodeActivity.this, setUpPasswordActivity.class);
+                    //now make it happen
+                    intent5.putExtra("user", user);
+                    Log.d("PIN ENTERED", pinNumber.getText().toString().trim());
+                    verifyUser(user,intent5,pinNumber.getText().toString().trim());
                 }
-                Intent intent6 = getIntent();
-                User user = (User)intent6.getSerializableExtra("user");
-                //declare where you intend to go
-                Intent intent5 = new Intent(verificationCodeActivity.this, setUpPasswordActivity.class);
-                //now make it happen
-                intent5.putExtra("user", user);
-                startActivity(intent5);
+
+
             }
         });
 
@@ -128,4 +187,48 @@ public class verificationCodeActivity extends AppCompatActivity {
             }
         });
     }
+
+
+    public void verifyUser(User user, final Intent intent, String PIN) {
+        String countryCode = user.getPhoneCountryCode();
+        String phoneNumber = user.getNexmoPhoneNumber();
+        verifyClient.checkPinCode(PIN); //This verifies the PIN.
+        //checkPinCode has no return value, thus in order to determine if the user has been verified,
+        //we must check the user's status manually to determine success.
+        verifyClient.getUserStatus(countryCode, phoneNumber, new SearchListener() {
+            @Override
+            public void onException(IOException exception) {
+                //TODO: Add Exception Code
+            }
+
+            @Override
+            public void onUserStatus(UserStatus userStatus) {
+                if (userStatus == UserStatus.USER_VERIFIED)
+                    startActivity(intent);
+                else {
+                    Toast.makeText(getApplicationContext(),"Wrong PIN Entered", Toast.LENGTH_SHORT);
+                    return;
+                }
+            }
+
+            @Override
+            public void onError(VerifyError errorCode, String errorMessage) {}
+        });
+    }
+
+    public void logOut(String phnNum, String code) {
+        verifyClient.command(code, phnNum, Command.LOGOUT, new CommandListener() {
+            @Override
+            public void onSuccess(Command command) {
+                // successfully logged out user
+                // Yay.
+            }
+            //I'm sure these guys have their uses, but I'm not gonna bother right now.
+            @Override
+            public void onError(Command command, com.nexmo.sdk.verify.event.VerifyError verifyError, String s) {}
+            @Override
+            public void onException(IOException e) {}
+        });
+    }
+
 }
