5cfd012a279efacef89853e7c72264a1bc8a2d33
Anthony John Kowal III <ajkowal@asu.edu>
2017-04-20 13:12:31 -0700

    Commit Out Due to Errors

--- app/src/main/java/com/skoovy/android/GeofenceTransitionsIntentService.java
+++ app/src/main/java/com/skoovy/android/GeofenceTransitionsIntentService.java
@@ -1,73 +1,21 @@
-package com.skoovy.android;
-
-import com.google.android.gms.location.Geofence;
-import java.util.ArrayList;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.firebase.database.FirebaseDatabase;
-
-import android.app.IntentService;
-import android.app.PendingIntent;
-import android.content.Intent;
-import android.content.SharedPreferences;
-import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
-
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
-import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
-import com.google.android.gms.common.api.ResultCallback;
-import com.google.android.gms.common.api.Status;
-import com.google.android.gms.location.Geofence;
-import com.google.android.gms.location.GeofencingRequest;
-import com.google.android.gms.location.GeofencingEvent;
-import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.location.GeofencingApi;
-import com.google.android.gms.maps.model.LatLng;
-
-import java.util.ArrayList;
-import java.util.Map;
-
-import android.app.IntentService;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
-import android.content.Context;
-import android.content.Intent;
-import android.graphics.BitmapFactory;
-import android.graphics.Color;
-import android.support.v4.app.NotificationCompat;
-import android.support.v4.app.TaskStackBuilder;
-import android.text.TextUtils;
-import android.util.Log;
-
-import com.google.android.gms.location.Geofence;
-import com.google.android.gms.location.GeofencingEvent;
-
-import java.util.ArrayList;
-import java.util.List;
+//package com.skoovy.android;
 
 /**
  * Created by Lilith on 4/19/2017.
  */
 
-public class GeofenceTransitionsIntentService extends IntentService{
+//public class GeofenceTransitionsIntentService extends IntentService{
 
 //    public GeofenceTransitionsIntentService(){
 //        super("GeofenceTransitionsIntentService");
 //    }
 
-    protected static String geofence_transition_entered = "entered";
-    protected static String geofence_transition_exited = "exit";
-    protected static String unknown_geofence_transition = "unknown";
-    protected static String geofence_transition_invalid_type = "invalid";
+ //   protected static String geofence_transition_entered = "entered";
+ //   protected static String geofence_transition_exited = "exit";
+ //   protected static String unknown_geofence_transition = "unknown";
+  //  protected static String geofence_transition_invalid_type = "invalid";
    // protected static final String TAG = "GeofenceTransitionsIS";
-    protected static final String geofence_transition_notification_text = "Transition notification";
+   // protected static final String geofence_transition_notification_text = "Transition notification";
 
 //    protected ArrayList<Geofence> mGeofenceList;
 //
@@ -186,14 +134,14 @@ public class GeofenceTransitionsIntentService extends IntentService{
 //        }*//*
 //    }
 
-
+/*
     protected static final String TAG = "GeofenceTransitionsIS";
 
     /**
      * This constructor is required, and calls the super IntentService(String)
      * constructor with the name for a worker thread.
      */
-    public GeofenceTransitionsIntentService() {
+   /* public GeofenceTransitionsIntentService() {
         // Use the TAG to name the worker thread.
         super(TAG);
     }
@@ -208,6 +156,7 @@ public class GeofenceTransitionsIntentService extends IntentService{
      * @param intent sent by Location Services. This Intent is provided to Location
      *               Services (inside a PendingIntent) when addGeofences() is called.
      */
+   /*
     @Override
     protected void onHandleIntent(Intent intent) {
         GeofencingEvent geofencingEvent = GeofencingEvent.fromIntent(intent);
@@ -252,6 +201,7 @@ public class GeofenceTransitionsIntentService extends IntentService{
      * @param triggeringGeofences   The geofence(s) triggered.
      * @return                      The transition details formatted as String.
      */
+   /*
     private String getGeofenceTransitionDetails(
             Context context,
             int geofenceTransition,
@@ -273,6 +223,7 @@ public class GeofenceTransitionsIntentService extends IntentService{
      * Posts a notification in the notification bar when a transition is detected.
      * If the user clicks the notification, control goes to the MainActivity.
      */
+   /*
     private void sendNotification(String notificationDetails) {
         // Create an explicit content Intent that starts the main Activity.
         Intent notificationIntent = new Intent(getApplicationContext(), MapsActivity.class);
@@ -321,6 +272,7 @@ public class GeofenceTransitionsIntentService extends IntentService{
      * @param transitionType    A transition type constant defined in Geofence
      * @return                  A String indicating the type of transition
      */
+  /*
     private String getTransitionString(int transitionType) {
         switch (transitionType) {
             case Geofence.GEOFENCE_TRANSITION_ENTER:
@@ -332,4 +284,4 @@ public class GeofenceTransitionsIntentService extends IntentService{
         }
     }
 }
-
+*/
